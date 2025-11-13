cfed4a36e137309254f320db61fd4f8489715237
mahdimahdi1995 <mahdimahdi1995.sy@gmail.com>
2025-11-13 01:03:45 +0100

    end of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = signal('client');
+export class AppComponent implements OnInit {
+  basUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+  title = signal('Skinet');
+  products: Product[] = [];
+
+  ngOnInit() {
+    this.http.get<Pagination<Product>>(this.basUrl + 'products').subscribe({
+      next: response => {
+        this.products = response.data;
+        console.log(response);
+      },
+      error: error => {
+        console.error(error);
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    });
+  }
 }
